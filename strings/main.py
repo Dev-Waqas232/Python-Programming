d558a477983922f598d6c186271f889fd29bd5ec
Dev-Waqas232 <devwaqas232@gmail.com>
2025-11-02 19:27:13 +0500

    learn: strings reverse and floats

---
+++ strings/main.py
@@ -0,0 +1,9 @@
+# In python, strings are immutable
+
+name = "Waqas"
+
+print(name[::-1])  # Trick to reverse the thing by setting the step to -1
+
+float_num = 0.888392398293828939283820
+
+print(f"{float_num: .4f}")  # Specifying percision for a float
